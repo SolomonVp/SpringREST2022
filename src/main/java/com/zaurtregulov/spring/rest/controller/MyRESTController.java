1f026d8ffbcf8b2e15965dd57d0c004284092f96
SolomonVP <Lum7023@yandex.ru>
2022-11-07 00:09:09 +0300

    add POSTmethod 'updateEmployee' ключевую роль играет id

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -41,6 +41,12 @@ public class MyRESTController {
         employeeService.saveEmployee(employee);
         return employee;
     }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
 }
 
 
